3e3044ac9a85dc6eca14113704fe8c3adc02cc5d
qilei <qilei@daokoudai.com>
2018-04-05 15:24:48 +0800

    get ratio

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ class Mosaic {
 	constructor (opt) {
 		// 合并参数
 		this.opt = Object.assign({},this.getDefaultOpt(),opt);
+		// 获取比例
+		this.pixelRatio = this.getPixelRatio(document.createElement('canvas').getContext('2d'));
 		// 创建canvas
 		this.createMosaicCanvas(opt.el);
 	}
@@ -18,8 +20,8 @@ class Mosaic {
 		const canvasArr = ['canvasBg','canvasOpe','canvasArea']
 		canvasArr.forEach((currVal,index,arr)=>{
 			this[currVal] = this.createCanvasArrFun(currVal,el)
+			this[`${currVal}Ctx`] = this[currVal].getContext('2d')
 		})
-
 	}
 	createCanvasArrFun(idStr,parent){
 		let canvas = document.createElement('canvas');
@@ -28,9 +30,20 @@ class Mosaic {
 		canvas.style.position = 'absolute';
 		canvas.style.top = 0;
 		canvas.style.left = 0;
+		canvas.setAttribute('height',parseInt(this.opt.height)*2)
+		canvas.setAttribute('width',parseInt(this.opt.width)*2)
 		canvas.setAttribute('id',idStr);
 		parent.appendChild(canvas);
 		return canvas
+	}
+	getPixelRatio(context) {
+	  	let backingStore = context.backingStorePixelRatio ||
+	    	context.webkitBackingStorePixelRatio ||
+	    	context.mozBackingStorePixelRatio ||
+	    	context.msBackingStorePixelRatio ||
+	    	context.oBackingStorePixelRatio ||
+	    	context.backingStorePixelRatio || 1;
+   		return (window.devicePixelRatio || 1) / backingStore;
 	}
 }
 const opt = {
